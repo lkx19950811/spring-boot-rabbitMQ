22c2724b0f7ea61a449f925f2e9ef09477dc3338
lkx <lkx19950811@163.com>
2018-09-13 18:16:09 +0800

    回家继续看

--- src/main/java/com/lkx/rabbitdemo/producer/UserSend1.java
+++ src/main/java/com/lkx/rabbitdemo/producer/UserSend1.java
@@ -4,7 +4,6 @@ import com.lkx.rabbitdemo.RandomMock;
 import com.lkx.rabbitdemo.pojo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
